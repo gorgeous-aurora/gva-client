c06cd27d3c2777bf802cf12c11fb7aa410ee3fe4
pixelmaxQM <flipped_aurora@163.com>
2024-07-24 23:30:48 +0800

    first commit

---
+++ src/view/home/index.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div class="box text-red-900">
+    {{userStore.userInfo}}
+    asdasd
+  </div>
+</template>
+
+<script setup>
+import {useUserStore} from "@/pinia/modules/user.js"
+import {captcha} from "@/api/user.js"
+const userStore = useUserStore()
+captcha({})
+</script>
+
+
+<style scoped  lang="scss">
+
+
+</style>
